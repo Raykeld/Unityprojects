80633888a39248749eb0415c292efb469b00f7f4
Unknown <sammclain5@gmail.com>
2017-01-31 11:15:14 -0700

    commiting to classes branch
    Move and rotate code

---
+++ New Unity Project/Assets/rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (.5f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime * speed);	
+	}
+}
